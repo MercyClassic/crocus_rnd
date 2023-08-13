b9ff3f1867e1ad6540231c8ef3278e96d1d21fea
Mercy <mercyclassic123@gmail.com>
2023-08-13 04:23:03 +0300

    add smooth move to catalog

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,11 +1,27 @@
-import {Link} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, Link, useSearchParams } from 'react-router-dom';
 import Product from './Product';
 
 
 const ProductList = ({data}) => {
+    const location = useLocation();
+    const [searchParams, setSearchParams] = useSearchParams();
     const products = data.result;
     const cart_products = data.cart_products;
     const favourites = data.favourites;
+
+    const onLoad = () => {
+        if (location.search.includes('&catalog')) {
+            let url = window.location.href.replace('&catalog', '');
+            searchParams.delete('catalog')
+            setSearchParams(searchParams)
+            document.getElementById('catalog').scrollIntoView({behavior: 'smooth'});
+        }
+    }
+
+    useEffect(() => {
+        onLoad();
+    }, [location.search])
 
     return(
         <section className="flower">
